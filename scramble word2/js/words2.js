4842401d9286e7167fd2c3bba9265678df7215b3
kamakship18 <kamakshipandoh8@gmail.com>
2023-11-22 16:09:51 +0530

    change in js

--- scramble word2/js/words2.js
+++ scramble word2/js/words2.js
@@ -1,26 +1,30 @@
 let words =[
-{
-    word: "garden",
-    hint: "Space for planting flower and plant"
-},
-{
-    word: "position",
-    hint: "Location of someone or something"
-},
-{
-    word: "feather",
-    hint: "Hair like outer covering of bird"
-},
-{
-    word: "comfort",
-    hint: "A pleasant feeling of relaxation"
-},
-{
-    word: "tongue",
-    hint: "The muscular organ of mouth"
-},
-{
-    word: "expansion",
-    hint: "The process of increase or grow"
-}
+    {
+        word: "canvas",
+        hint: "Piece of fabric for oil painting"
+    },
+    {
+        word: "garden",
+        hint: "Space for planting flower and plant"
+    },
+    {
+        word: "position",
+        hint: "Location of someone or something"
+    },
+    {
+        word: "feather",
+        hint: "Hair like outer covering of bird"
+    },
+    {
+        word: "comfort",
+        hint: "A pleasant feeling of relaxation"
+    },
+    {
+        word: "country",
+        hint: "A politically identified region"
+    },
+    {
+        word: "group",
+        hint: "A number of objects or persons"
+    },
 ]
---
+++ scramble word3/js/words3.js
@@ -0,0 +1,34 @@
+let words=[
+    {
+        word: "field",
+        hint: "Area of land for farming activities"
+    },
+    {
+        word: "friend",
+        hint: "Person other than a family member"
+    },
+    {
+        word: "pocket",
+        hint: "A bag for carrying small items"
+    },
+    {
+        word: "needle",
+        hint: "A thin and sharp metal pin"
+    },
+    {
+        word: "taste",
+        hint: "Ability of tongue to detect flavour"
+    },
+    {
+        word: "expert",
+        hint: "Person with extensive knowledge"
+    },
+    {
+        word: "statement",
+        hint: "A declaration of something"
+    },
+    {
+        word: "store",
+        hint: "Large shop where goods are traded"
+    },
+]
