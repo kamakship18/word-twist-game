a00ee1bf6e0372bc2d0878c0892978c648439532
kamakship18 <kamakshipandoh8@gmail.com>
2023-11-22 12:46:44 +0530

    instruction page added

--- front-page/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('instructionsButton').addEventListener('click', function() {
-    var instructionsText = document.getElementById('instructionsText');
-    if (instructionsText.style.display === 'none') {
-        instructionsText.style.display = 'block';
-    } else {
-        instructionsText.style.display = 'none';
-    }
-});
